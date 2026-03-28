c7cc480b8f8f248550983958285c21f5b9a8e10b
Paula Pires <paula.pires2640@gmail.com>
2026-03-27 21:54:37 -0300

    adicionando os resouces (signin, login, forgot, redefine_password)

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_restful import Api, Resource
-
+from backend import signin, login, forgot, redefine_password
 app = Flask(__name__)
 api = Api(app)
 app.secret_key = 'projeto_secreto'
 
-api.add_resource(,'/')
+api.add_resource(signin,'/signin')
+api.add_resource(login,'/login')
+
+api.add_resource(forgot,'/forgot')
+api.add_resource(redefine_password,'/redefine_password')
 
 if __name__ == "__main__":
     app.run(debug=True)
