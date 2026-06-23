befcc533865f718b620c6dffeefbbf22f99d4ce6
Larissa Maiero <larissamaiero@gmail.com>
2026-06-22 21:28:58 -0300

    update fetch URL for profile update to relative path

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1369,7 +1369,7 @@ document.addEventListener('DOMContentLoaded', function () {
             if (fotoTemp) formData.append('foto', fotoTemp);
 
             try {
-                const res = await fetch('http://127.0.0.1:5000/update_perfil', {
+                const res = await fetch('/update_perfil', {
                     method: 'POST',
                     body: formData
                 });
