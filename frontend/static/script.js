2ba2d7644668600bfe8c481bd69ec5f3b6865fc1
Paula Pires <paula.pires2640@gmail.com>
2026-06-22 22:35:22 -0300

    aaaa

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1369,6 +1369,17 @@ document.addEventListener('DOMContentLoaded', function () {
             if (fotoTemp) formData.append('foto', fotoTemp);
 
             try {
+                if (fotoTemp != null) {
+                    const res = await fetch('/salvar_foto', {
+                        method: 'POST',
+                        body: formData
+                    });
+                    const data_foto = await res.json();
+
+                    if (data_foto.status === 'error') { mostrarToast(data_foto.mensagem, 'error'); return; }
+                        
+                }
+                
                 // O da foto
                 const res = await fetch('/salvar_foto', {
                     method: 'POST',
