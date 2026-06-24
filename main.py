ab514c42a4755cf95eea85a7143db5044420439c
Paula Pires <paula.pires2640@gmail.com>
2026-06-24 17:03:44 -0300

    TESTE

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource
-from backend.resources.auth import update_Password, signin, login, salvar_foto, salvar_video, editar_bio, editar_nome, forgot, upload,redefine_password,delete_Account,bloquear, logout,desbloquear, check_login , dados_config, search,translate, resend_code, check_codigo, google
+from backend.resources.auth import update_Password, signin, login, salvar_foto, salvar_video, editar_bio, editar_nome, forgot,redefine_password,delete_Account,bloquear, logout,desbloquear, check_login , dados_config, search,translate, resend_code, check_codigo, google
 from dotenv import load_dotenv
 from authlib.integrations.flask_client import OAuth
 import os
@@ -64,7 +64,7 @@ api.add_resource(logout, '/logout')
 api.add_resource(bloquear, '/bloquear')
 api.add_resource(desbloquear, '/desbloquear')
 api.add_resource(update_Password, '/update')
-api.add_resource(upload,'/upload')
+#api.add_resource(upload,'/upload')
 api.add_resource(salvar_video, '/salvar_video')
 api.add_resource(salvar_foto, '/salvar_foto')
 
