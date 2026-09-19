95f7d129a8fd1d9dd7ff033b2a30b22807917a32
Paula Pires <paula.pires2640@gmail.com>
2026-09-19 20:34:37 -0300

    Configurando para rodar localmente

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -54,13 +54,13 @@ def connection():
         Função responsável por fazer inicializar a conexão com o MySQL do railway
     """
     return pymysql.connect (
-        port=3306,
-        host='mysql.railway.internal',
-        #host='localhost',
+        #port=3306,
+        #host='mysql.railway.internal',
+        host='localhost',
         user='root',
         password=bd_password,
-        database='railway',
-        #database='twitch',
+        #database='railway',
+        database='twitch',
         cursorclass=pymysql.cursors.Cursor
     )
 
